5cdf6bdead910497a377b3e56ce6bcda01c571d3
Fedor Pereverzev <maphtown@gmail.com>
2018-03-22 15:43:57 +0300

    избавился от eval()

---
+++ script.js
@@ -0,0 +1,47 @@
+const display = document.getElementById('display');
+
+
+
+
+const antiEval = (str) => {
+    
+  let arr = str.split(' ');
+    
+  switch(arr[1]){
+    case '+':  
+    return +arr[0] + +arr[2];
+    break;
+    case '-':
+    return +arr[0] - +arr[2];
+    break;
+    case '/':  
+    return +arr[0] / +arr[2];
+    break;
+    case '*':
+    return +arr[0] * +arr[2];
+    break;      
+  };
+};
+
+
+const keyboard = document.getElementById('keyboard');
+  let str = '';
+keyboard.addEventListener('click', (e) => {
+  
+    let target = e.target;
+    if(target.value.match(/[0123456789]/)){
+    display.textContent += target.value;
+        str += target.value;
+    };
+    
+    if(target.value.match(/[+-/\///\*/]/)){
+        str += ' ' + target.value + ' ';
+    }
+    
+    if(target.value.match('=')){
+        display.textContent = antiEval(str);
+        str = antiEval(str);
+    };
+})
+
+
