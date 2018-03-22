71a83e62e30336948fb6e529c4f3af79196fce4c
Fedor Pereverzev <maphtown@gmail.com>
2018-03-22 19:19:38 +0300

    добавлена очистка 'c'

--- script.js
+++ script.js
@@ -19,14 +19,19 @@ const antiEval = (str) => {
     break;
     case '*':
     return +arr[0] * +arr[2];
-    break;      
+    break;
+    //case '%':
+    //return parseFloat(+arr[0] / 100).toFixed(2);
+      //    return +arr[0] / 100;
+    //break;      
   };
 };
 
 let booleanSign = true;
-
+let count = 0;
 const keyboard = document.getElementById('keyboard');
   let str = '';
+
 keyboard.addEventListener('click', (e) => {
   
     let target = e.target;
@@ -35,22 +40,36 @@ keyboard.addEventListener('click', (e) => {
         if (booleanSign === true){
             display.textContent = '';
              booleanSign = false;
-        }
+        };
+        
     display.textContent += target.value;
-        str += target.value;
-       
+        str += target.value;  
     };
     
-    if(target.value.match(/[^0123456789\.]/)){
+    if(target.value.match(/[^0123456789\.=c]/)){
+        count += 1;
+        if(count === 2){
+            display.textContent = antiEval(str);
+        str = antiEval(str);
+            count = 1;
+        };
         str += ' ' + target.value + ' ';
+        booleanSign = true; 
+       
+    };
+     
+    if(target.value.match('c')){
+        count = 0;
         booleanSign = true;
+        str = ''
+        display.textContent = '0';
         
-    }
+    };
     
     if(target.value.match('=')){
         display.textContent = antiEval(str);
         str = antiEval(str);
     };
-})
+});
 
 
